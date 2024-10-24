c61b174d7f56026ca8abf9cabbc4e1f57897ea33
eduardoocosta <e.costa5@alunoescolassociais.org.br>
2024-10-24 12:10:41 -0300

    Inserir alterações

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# conversao_de_moedas
+# Conversor de moedas
---
+++ conver.js
@@ -0,0 +1,16 @@
+document.getElementById ('currency-converter').addEventListener('submit', function(event){
+    event.preventDefault();
+
+    // Obter valores de entrada informados pelo usuário
+    const valor = parseFloat(document.getElementById('amount').value);
+    const daMoeda = document.getElementById('daMoeda').value;
+    const paraMoeda = document.getElementById('paraMoeda').value;
+
+    // Definir taxas de câmbio fixas
+    const exchangeRates = {
+        USD: { BRL: 5.70, EUR: 0.93 },
+        BRL: { USD: 0.18, EUR: 0.16 },
+        EUR: { USD: 1.08, EUR: 6.16 },
+    };
+
+});
